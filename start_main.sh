95a7a47d8e577285b3b424d74d3e4ce54f80bd73
wangxiaowu9527 <wangxiaowu_9527@163.com>
2021-09-05 15:30:18 +0800

    first_commit

--- start_main.sh
+++ start_main.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
+function start(){
+    uwsgi wsgi.ini
+}
+
 # If monitor kafka,you need set the JAVA_HOME
 # Then start app with this script
 # set JAVA_HOME to your own
 # readonly JAVA_HOME="/usr/local/jdk1.8.0_221"
-
 if [[  "X$JAVA_HOME" != "X" ]]; then
     PATH="${JAVA_HOME}/bin:$PATH"
     if [[ -d ${JAVA_HOME}/bin ]]; then
-       python3 main.py
+       start
     fi
 else
-    # windows cannot use uwsgi,you can start app by command: python3 main.py
-    uwsgi wsgi.ini
+    start
 fi
-
 
